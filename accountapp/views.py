4cd1a9f320588f117a4b10fd45340be76617a2f4
uthem150 <uthem150@gmail.com>
2024-02-24 03:14:45 +0900

    include/extends/block구문을 이용한 뼈대 html

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html') #accountapp 폴더 내부의 hello_world파일 가져오기 (header, footer는 변하지x)
 
