1d42a1f16cb9d40a0c8735336d1e773ddd8caabb
uthem150 <uthem150@gmail.com>
2024-02-24 20:49:25 +0900

    인증 시스탬 구축

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
@@ -15,18 +15,21 @@ from accountapp.models import HelloWorld
 
 def hello_world(request):
 
-    if request.method == "POST":
+    if request.user.is_authenticated: #로그인을 해야만, 게시글을 볼 수 있도록
+        if request.method == "POST":
 
-        temp = request.POST.get('hello_world_input') #request에서 post메소드 중에서 hello_world_input이라는 이름을 가진 데이터를 가져오기
+            temp = request.POST.get('hello_world_input') #request에서 post메소드 중에서 hello_world_input이라는 이름을 가진 데이터를 가져오기
 
-        new_hello_world = HelloWorld() #models.py의 HelloWorld에서 나온 새로운 객체가 저장됨.
-        new_hello_world.text = temp #텍스트 필드 안에 받은 값 넣어주기
-        new_hello_world.save() #db에 데이터를 저장시키고 있음.
+            new_hello_world = HelloWorld() #models.py의 HelloWorld에서 나온 새로운 객체가 저장됨.
+            new_hello_world.text = temp #텍스트 필드 안에 받은 값 넣어주기
+            new_hello_world.save() #db에 데이터를 저장시키고 있음.
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world')) #reverse('accountapp:hello_world')는 Django에서 URL 패턴 이름을 기반으로 해당 URL을 생성하는 함수
+            return HttpResponseRedirect(reverse('accountapp:hello_world')) #reverse('accountapp:hello_world')는 Django에서 URL 패턴 이름을 기반으로 해당 URL을 생성하는 함수
+        else:
+            hello_world_list = HelloWorld.objects.all()  # 모든 HelloWorld 객체들을 불러옴
+            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list}) #accountapp 폴더 내부의 hello_world파일 가져오기 (header, footer는 변하지x)
     else:
-        hello_world_list = HelloWorld.objects.all()  # 모든 HelloWorld 객체들을 불러옴
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list}) #accountapp 폴더 내부의 hello_world파일 가져오기 (header, footer는 변하지x)
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 # 장고의 제너릭에 있는 CreateView를 상속받아서 사용
 class AccountCreateView(CreateView) :
@@ -47,8 +50,32 @@ class AccountUpdateView(UpdateView) :
     success_url = reverse_lazy('accountapp:hello_world') #폼 제출 후에 이동할 URL을 지정
     template_name = 'accountapp/update.html' #사용될 템플릿 파일의 경로를 지정
 
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().post(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().post(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
