141e13c647c8cda07dbd0c62746fe6d90bc3a3dc
uthem150 <uthem150@gmail.com>
2024-02-24 13:49:07 +0900

    POST통신을 이용한 DB데이터 저장

--- accountapp/models.py
+++ accountapp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-# Django에서 모델은 데이터베이스 테이블과의 상호작용을 관리하는데 사용되는 객체 (데이터베이스에 저장될 데이터)
+# Django에서 모델은 데이터베이스 테이블과의 상호작용을 관리하는데 사용되는 객체만드는 틀 (데이터베이스에 저장될 데이터)
 class HelloWorld(models.Model):
     text = models.CharField(max_length=255, null=False) #문자열 필드, 최대 길이 255, 필드가 비어 있을 수 없음
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,12 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accountapp.models import HelloWorld
+
+
 # Create your views here.
 
 
 def hello_world(request):
 
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!!'}) #accountapp 폴더 내부의 hello_world파일 가져오기 (header, footer는 변하지x)
+
+        temp = request.POST.get('hello_world_input') #request에서 post메소드 중에서 hello_world_input이라는 이름을 가진 데이터를 가져오기
+
+        new_hello_world = HelloWorld() #models.py의 HelloWorld에서 나온 새로운 객체가 저장됨.
+        new_hello_world.text = temp #텍스트 필드 안에 받은 값 넣어주기
+        new_hello_world.save() #db에 데이터를 저장시키고 있음.
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world}) #accountapp 폴더 내부의 hello_world파일 가져오기 (header, footer는 변하지x)
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
