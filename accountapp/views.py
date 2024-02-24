04f97b7cd4fbe8dfa28df394442150864fdfb3e3
uthem150 <uthem150@gmail.com>
2024-02-24 13:30:29 +0900

    GET, POST

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,5 +5,8 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html') #accountapp 폴더 내부의 hello_world파일 가져오기 (header, footer는 변하지x)
 
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!!'}) #accountapp 폴더 내부의 hello_world파일 가져오기 (header, footer는 변하지x)
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
