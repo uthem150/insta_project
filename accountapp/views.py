d986434e5817147f3caf63c12d87fecaf3349609
uthem150 <uthem150@gmail.com>
2024-02-24 14:08:17 +0900

    DB정보 접근 및 장고 탬플릿 내 for loop

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
 
@@ -17,6 +18,7 @@ def hello_world(request):
         new_hello_world.text = temp #텍스트 필드 안에 받은 값 넣어주기
         new_hello_world.save() #db에 데이터를 저장시키고 있음.
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world}) #accountapp 폴더 내부의 hello_world파일 가져오기 (header, footer는 변하지x)
+        return HttpResponseRedirect(reverse('accountapp:hello_world')) #reverse('accountapp:hello_world')는 Django에서 URL 패턴 이름을 기반으로 해당 URL을 생성하는 함수
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()  # 모든 HelloWorld 객체들을 불러옴
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list}) #accountapp 폴더 내부의 hello_world파일 가져오기 (header, footer는 변하지x)
