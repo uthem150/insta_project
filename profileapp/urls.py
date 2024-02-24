34a70edf25979c3524691cdb0bbe03621be20263
uthem150 <uthem150@gmail.com>
2024-02-24 23:55:50 +0900

    profileapp 구현 시작

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,6 +1,9 @@
+from django.urls import path
 
-app_name = 'profileapp'
+from profileapp.views import ProfileCreateView
 
-urlpatterns = [
+app_name = 'profileapp' #해당 앱의 이름을 정의
 
+urlpatterns = [ #URL 패턴들을 정의하는 리스트
+    path('create/', ProfileCreateView.as_view(), name='create'), #/create/ 경로에 대한 URL 패턴 정의('create/'은 http://도메인주소/create/에 해당하는 URL)
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
 
 # Create your views here.
+
+class ProfileCreateView(CreateView): #CreateView를 상속받아 프로필 생성에 대한 뷰를 정의하는 클래스
+    model = Profile
+    context_object_name = 'target_profile' #템플릿에서 사용할 컨텍스트 객체의 이름을 지정
+    form_class = ProfileCreationForm #뷰에서 사용할 폼 클래스를 지정
+    success_url = reverse_lazy('accountapp:hello_world') #프로필 생성이 성공한 후 이동할 URL을 지정
+    template_name = 'profileapp/create.html' #뷰에서 사용할 템플릿 파일의 경로를 지정
+
+    def form_valid(self, form):
+        temp_profile = form.save(commit=False) #임시 데이터 저장
+        temp_profile.user = self.request.user #temp_profile.user에 현재 요청을 보낸 사용자를 할당하고,
+        temp_profile.save() #프로필을 저장
+        return super().form_valid(form)
