451faa9de9f9057a5c735fed245c989d06f7f655
uthem150 <uthem150@gmail.com>
2024-02-24 14:37:51 +0900

    CreateView를 통한 회원가입 구현

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path #패턴을 설정하는 함수
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
@@ -8,5 +8,6 @@ app_name = "accountapp"
 # "accountapp:hello_world" -> accountapp내부에서 hello_world라는 이름의 주소로 바로 가라
 
 urlpatterns = [
-    path('hello_world/', hello_world, name="hello_world") #URL 패턴으로 사용할 경로, URL 패턴에 대해 호출될 함수 또는 뷰, 이 URL 패턴의 이름을 "hello_world"로 지정
+    path('hello_world/', hello_world, name="hello_world"), #URL 패턴으로 사용할 경로, URL 패턴에 대해 호출될 함수 또는 뷰, 이 URL 패턴의 이름을 "hello_world"로 지정
+    path('create/', AccountCreateView.as_view(), name="create"), #회원가입 경로 지정 (class based view는 함수형처럼 작동하게 하려면, as_view()붙여야 함)
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -22,3 +25,10 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()  # 모든 HelloWorld 객체들을 불러옴
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list}) #accountapp 폴더 내부의 hello_world파일 가져오기 (header, footer는 변하지x)
+
+# 장고의 제너릭에 있는 CreateView를 상속받아서 사용
+class AccountCreateView(CreateView) :
+    model = User #모델을 생성하는 데 사용될 모델 클래스를 지정
+    form_class = UserCreationForm # UserCreationForm을 사용하여 사용자 생성에 필요한 입력 폼을 정의
+    success_url = reverse_lazy('accountapp:hello_world') #폼 제출 후에 이동할 URL을 지정
+    template_name = 'accountapp/create.html' #사용될 템플릿 파일의 경로를 지정 (회원가입을 할 때 볼 html파일)
