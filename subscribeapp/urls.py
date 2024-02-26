1bd438e6f66ceac15681375bddcbbccd816200d5
uthem150 <uthem150@gmail.com>
2024-02-26 22:20:43 +0900

    Field Lookup을 사용한 구독 페이지 구현

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from subscribeapp.views import SubscriptionView
+from subscribeapp.views import SubscriptionView, SubscriptionListView
 
 app_name = 'subscribeapp'
 
@@ -8,4 +8,4 @@ app_name = 'subscribeapp'
 
 urlpatterns = [
     path('subscribe/', SubscriptionView.as_view(), name='subscribe'),
-]
+    path('list/', SubscriptionListView.as_view(), name='list'),]
--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -27,3 +28,15 @@ class SubscriptionView(RedirectView):
         else:
             Subscription(user=user, project=project).save() #Subscription 모델을 생성하여 사용자와 프로젝트를 연결하고 저장
         return super(SubscriptionView, self).get(request, *args, **kwargs) #부모 클래스인 RedirectView의 get 메서드를 실행합니다
+
+@method_decorator(login_required, 'get') #login한 상태에서만 SubscribeList볼 수 있음
+class SubscriptionListView(ListView):
+    model = Article # 이 뷰에서 사용할 데이터베이스 모델은 Article
+    context_object_name = 'article_list' #템플릿에서 사용할 객체 리스트의 컨텍스트 변수 이름은 'article_list'
+    template_name = 'subscribeapp/list.html'
+    paginate_by = 5
+
+    def get_queryset(self): #현재 사용자의 구독 프로젝트를 필터링하여 해당 프로젝트에 속하는 Article 객체들을 가져옴
+        projects = Subscription.objects.filter(user=self.request.user).values_list('project') #Subscription안에서 user가 self.request.user인 모든 구독정보를 가져오고, 리스트화
+        article_list = Article.objects.filter(project__in=projects) #fiels lookup을 이용하여, Article 모델에서 project 필드가 projects에 속하는 객체들을 필터링
+        return article_list
