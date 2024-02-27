63cc2b917cd19f6b9044cb65a790c3118066fb4c
uthem150 <uthem150@gmail.com>
2024-02-28 02:55:17 +0900

    배포용에 docker secrete 적용

--- instagram/settings/deploy.py
+++ instagram/settings/deploy.py
@@ -2,6 +2,13 @@
 
 from .base import *
 
+def read_secret(secret_name): #secrete_name을 받아서, 해당 이름을 가진 secrete를 불러와라
+    file = open('/run/secrets/' + secret_name) #open 함수를 사용하여 /run/secrets/ 경로와 secret_name을 결합하여 파일을 염
+    secret = file.read()
+    secret = secret.rstrip().lstrip() #읽어온 시크릿 문자열의 양쪽 공백을 제거합니다.
+    file.close()
+    return secret
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -17,7 +24,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY') #.env파일 안에서 SECRET_KEY를 읽어옴
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY') #.env파일 안에서 SECRET_KEY를 읽어옴
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -32,7 +39,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql", #mariadb를 쓰지만 mysql이라 씀(구성은 거의 같음)
         "NAME": "django",
         "USER": "django",
-        "PASSWORD": "password123",
+        "PASSWORD": read_secret('MYSQL_PASSWORD'),
         "HOST": "mariadb",
         "PORT": "3306",
     }
